98b252ede4679a313adb07f6bc90741e2633e0f9
Coolmesito <msamaniegocalderon@gmail.com>
2024-05-02 12:51:40 -0500

    Subiendo vistas de areas,maquinas y horarios(General,Presencial,Virtual,Salon,Maquina,Area)

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,35 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/areas', function () {
+    return view('areas.index');
+});
+
+Route::get('/maquinas', function () {
+    return view('maquinas.index');
+});
+
+Route::get('/horarioGeneral', function () {
+    return view('horarioGeneral.index');
+});
+
+Route::get('/horarioPresencial', function () {
+    return view('horarioPresencial.index');
+});
+
+Route::get('/horarioSalon', function () {
+    return view('horarioSalon.index');
+});
+
+Route::get('/horarioArea', function () {
+    return view('horarioArea.index');
+});
+
+Route::get('/horarioMaquina', function () {
+    return view('horarioMaquina.index');
+});
+
+Route::get('/horarioVirtual', function () {
+    return view('horarioVirtual.index');
+});
