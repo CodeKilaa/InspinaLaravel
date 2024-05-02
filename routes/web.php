7d02dcd891ba504a2b50e1b06045ab5f8342dbfa
CodeKilaa <xluixarturox123@gmail.com>
2024-05-02 12:59:57 -0500

    Agregando vista colaboradores y salones

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/salones', function () {
+    return view('salones.index');
+});
+
+Route::get('/colaboradores', function () {
+    return view('colaboradores.index');
+});
+Route::get('/horario-clases', function () {
+    return view('colaboradores.horario-clases');
+});
